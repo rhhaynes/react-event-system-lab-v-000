6034fc041e7820930d73c5f3035dfb8e5a9b5ba2
Randall Harris Haynes <harrishaynes@gmail.com>
2018-06-19 14:53:20 +0000

    automatically backed up by learn

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,10 @@
-// Code EyesOnMe Component Here
+import React from 'react';
+
+export default class EyesOnMe extends React.Component {
+  render() {
+    return (
+      <button>
+      </button>
+    );
+  }
+}
