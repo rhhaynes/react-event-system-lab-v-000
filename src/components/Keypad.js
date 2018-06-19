9771c0882ecf70a88b402799bbc34fc05ce2933a
Randall Harris Haynes <harrishaynes@gmail.com>
2018-06-19 15:01:16 +0000

    automatically backed up by learn

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,9 +1,10 @@
 import React from 'react';
 
 export default class Keypad extends React.Component {
+  message = () => console.log('Entering password...');
   render() {
     return (
-      //code
+      <input type="password" onKeyUp={this.}/>
     );
   }
 }
