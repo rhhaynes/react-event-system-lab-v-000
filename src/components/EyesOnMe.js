d11ca5e85965e10a84b71e77f6ae1cd3a96ed125
Randall Harris Haynes <harrishaynes@gmail.com>
2018-06-19 15:02:44 +0000

    automatically backed up by learn

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default class EyesOnMe extends React.Component {
   render() {
     return (
-      <button onClick={this.func}>
+      <button onBlur={this.blurFunc} onFocus={this.focusFunc}>
       </button>
     );
   }
